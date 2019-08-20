e7659848531424d24b60f5ffb2bd698b9866af46
Ricardo García <ricardo@studio505.cl>
2019-08-20 17:47:21 -0400

    fix: creacion de gif

--- lib/gif_timer/image_magick.rb
+++ lib/gif_timer/image_magick.rb
@@ -6,7 +6,7 @@ module GifTimer
     end
 
     def self.create_gif(frames:, delay:, output_path:)
-      `gm convert -delay #{delay} #{frames.join(' ')} -loop 0 #{output_path}`
+      `gm convert -delay #{delay} #{frames.join(' ')} -loop 0 -dispose previous #{output_path}`
     end
   end
 end
