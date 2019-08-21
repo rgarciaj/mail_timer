b9efa3f972449910e81046909c2a02b36f03bc2c
Ricardo García <ricardo@studio505.cl>
2019-08-21 12:03:40 -0400

    add: bash to start server

---
+++ server.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$1" != "start" ] && [ "$1" != "stop" ]
+then
+  echo "'$1' is not one of the allowed parameters (start, stop)"
+
+else
+  
+  if [ "$1" == "start" ]
+  then
+    puma -p 3030 -d --pidfile pid
+    echo "PID: $(cat ./pid)"
+
+  elif [ "$1" == "stop" ]
+  then
+    echo "Stopping server..."
+
+    if [ -f ./pid ]
+    then
+      kill -9 $(cat ./pid)
+      rm ./pid
+      echo "Server stop"
+
+    else
+      echo "PID not found! can't stop the server"
+      
+    fi
+
+  fi
+
+fi
