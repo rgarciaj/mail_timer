4db1e94855f5f964bd4030e44660ac5e49d41bd9
Ricardo García <ricardo@studio505.cl>
2019-08-21 10:40:10 -0400

    wip

--- timer_api.rb
+++ timer_api.rb
@@ -21,17 +21,20 @@ get '/api/timer.gif' do
   if show_days == "0" then show_days = false else show_days = true end
 
   if type == "dolar" then
-    end_time = DateTime.parse(date)
+    end_time = DateTime.parse(date + " 00:00:00.0 -0400")
+    puts "-- DateTime.parse: #{end_time}"
     end_time = end_time + 1 # plus one day
     end_time = end_time + (8/24.0) # plus 8 hours
+    puts "-- end_time: #{end_time}"
     end_time = end_time.to_time.to_i
   elsif type.nil? then 
-    end_time = Time.parse(y + "-" + m + "-" + d + " " + hh + ":" + mm).to_i
+    end_time = Time.parse(y + "-" + m + "-" + d + " " + hh + ":" + mm,"-04:00").to_i
   end
 
   # generate file for timestamp and store local dir
   start_time = Time.now.to_i
-  
+  puts "-- start_time: #{Time.now}"
+
   time_difference = end_time - start_time
   if start_time > end_time
     time_difference = 0 # handle finished timer case
